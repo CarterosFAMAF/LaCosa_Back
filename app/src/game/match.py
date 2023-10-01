de04029ea981397adcd04c8fa7c6d9d2756bb10e
mario <Mario Picasso>
2023-10-01 05:25:14 -0300

    websocket connection andando, se rompe cuando te desconectas

--- app/src/game/match.py
+++ app/src/game/match.py
@@ -1,15 +1,20 @@
+import json
+
 from typing import List
 from fastapi import WebSocket
 from pony.orm import *
 
 from app.src.game.player import Player
-from app.src.router.match_connection_manager import MatchConnectionManager
+from app.src.game.match_connection_manager import MatchConnectionManager
 from app.src.models.base import Match as MatchDB
 from app.src.models.base import Player as PlayerDB
 
 
-# match tiene si o si owner, y si o si tiene 1 player
 class Match:
+    _id: int
+    _match_connection_manager: MatchConnectionManager
+    _player_owner_id: int
+
     def __init__(
         self, player_name: str, match_name: str, max_players: int, min_players: int
     ):
@@ -26,6 +31,7 @@ class Match:
 
             match_db = MatchDB(
                 name=match_name,
+                lobby_id=len(MATCHES),
                 number_players=1,
                 max_players=max_players,
                 min_players=min_players,
@@ -37,29 +43,32 @@ class Match:
             )
             flush()
 
-
             # set match id to return to client
             self._id = match_db.id
 
             # now that match is in db, update match player field
             player_db.match = match_db
             flush()
-        MATCHS.append(self)
-        
-    def join_match(player_name : str,match_id : int):
-        #create model player in the db
+        MATCHES.append(self)
+
+    def join_match(player_name: str, match_id: int):
+        # create model player in the db
         player = Player(player_name)
 
         with db_session:
-            player_db = PlayerDB.get(id = player._id)
-            match_db = MatchDB.get(id = match_id) 
+            player_db = PlayerDB.get(id=player._id)
+            match_db = MatchDB.get(id=match_id)
             match_db.players.add(player_db)
             match_db.number_players += 1
             flush()
 
+        return {"player_id": player_db.id, "match_name": match_db.name}
+
+    def get_match_lobby_id(match_id: int):
+        print(match_id)
+        with db_session:
+            match_db = MatchDB.get(id=match_id)
+            return match_db.lobby_id
 
 
-        return {"player_id": player_db.id,"match_name": match_db.name}
-    
-   
-MATCHS: List[Match] = []
+MATCHES: List[Match] = []
--- app/src/router/match_router.py
+++ app/src/router/match_router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, WebSocket, WebSocketDisconnect
 
+from app.src.game.player import Player
+from app.src.game.match import Match, MATCHES
+from app.src.models.schemas import MatchIn, MatchOut, JoinMatchOut
 
-from app.src.game.match import Match, MATCHS
-from app.src.router.schemas import MatchIn, MatchOut , JoinMatchOut
-from app.src.game.match import Match
 
 router = APIRouter()
 
@@ -24,35 +24,48 @@ async def create_match(match: MatchIn):
         result="Match created",
     )
 
-@router.post("/matches/{match_id}/join")
-async def join_match_endpoint(player_name: str,match_id : int):
-    #necesito traer la clase match.
-    match_out = Match.join_match(player_name,match_id)
 
-    return JoinMatchOut ( 
-        player_id = match_out["player_id"],
-        match_name = match_out["match_name"] )
+@router.post("/matches/{match_id}/join")
+async def join_match_endpoint(player_name: str, match_id: int):
+    # necesito traer la clase match.
+    match_out = Match.join_match(player_name, match_id)
+
+    return JoinMatchOut(
+        player_id=match_out["player_id"], match_name=match_out["match_name"]
+    )
+
 
 # websocket endpoint
-"""
-@router.websocket("/ws/matchs/{match_id}/{player_id}")
+
+
+@router.websocket("/ws/matches/{match_id}/{player_id}")
 async def websocket_endpoint(websocket: WebSocket, match_id: int, player_id: int):
-    # seteo variables para usar aca
-    match = MATCHS[match_id]
-    player = match._players[player_id]
-    manager = match.match_connection_manager
+    # TODO: move validation to a function in schemas.py?
+    print(f"{match_id} {match_id} {match_id} {match_id} {match_id}")
 
-    # seteo websocket en el objeto jugador para a futuro poder mandarle algo privado
-    player.websocket = websocket
+    try:
+        player = Player.get_player_by_id(player_id)
+    except:
+        raise "Player not found"
+    if player == None:
+        raise "Player not found"
 
-    await manager.connect(websocket)
+    try:
+        lobby_id = Match.get_match_lobby_id(match_id)
+    except:
+        raise "Match not found"
+    if lobby_id == None:
+        raise "Match not found"
+
+    match = MATCHES[lobby_id]
+
+    manager = match._match_connection_manager
+
+    await manager.connect(websocket, player_id)
+
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{player.name} says: {data}")
-    except WebSocketDisconnect:
-        await manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{player.name} left the chat")
+            await websocket.receive()
 
-"""
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
