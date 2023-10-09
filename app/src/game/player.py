726dd9dc357b0141ba13d9f4f74837b08caa32e2
Agus <rodrigo.quevedo@mi.unc.edu.ar>
2023-10-09 12:26:57 -0300

    update

--- app/src/game/player.py
+++ app/src/game/player.py
@@ -1,6 +1,7 @@
 from fastapi import WebSocket
 from app.src.models.base import Player as PlayerDB
-from app.src.models.base import *
+from app.src.models.base import Match as MatchDB
+from app.src.models.base import Card as CardDB
 from pony.orm import *
 
 
@@ -21,9 +22,9 @@ def get_player_by_id(player_id):
 def discard_card_of_player(card_id,match_id,player_id):
     with db_session:
         player = get_player_by_id(player_id)
-        card = Card.get(id = card_id)
-        match = Match.get(id = match_id)
+        card = CardDB.get(id = card_id)
+        match = MatchDB.get(id = match_id)
 
-        #player.hand.remove(card)
-        #match.discard_pile.add(card)
+        player.hand.remove(card)
+        match.discard_pile.add(card)
         flush()
