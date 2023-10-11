076c6e0236df6b9f2772485afd576560bf66bba4
Facu M <fnmazzucco@mi.unc.edu.ar>
2023-10-11 16:29:59 -0300

    CAR-35 remove cards from deck

--- app/src/game/match.py
+++ app/src/game/match.py
@@ -166,6 +166,7 @@ def deal_cards(match_id: int):
     for player in players_list:
         cards = select(c for c in match.deck).random(4)
         player.hand.add(cards)
+        match.deck.remove(cards)
     the_thing_player = select(p for p in match.players).random(1)[0]
     card = select(p for p in the_thing_player.hand).random(1)
     the_thing_player.hand.remove(card)
