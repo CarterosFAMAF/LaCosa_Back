7e4015b64bf018bd48ff4f1f5eefda69899c53bd
Agus <rodrigo.quevedo@mi.unc.edu.ar>
2023-10-19 15:28:58 -0300

    update funcando, a pleno

--- app/src/router/match_router.py
+++ app/src/router/match_router.py
@@ -79,6 +79,7 @@ async def join_match_endpoint(input: JoinMatchIn):
 
 @router.put(
     "/matches/{match_id}/players/{player_in_id}/{player_out_id}/{card_id}/play_card",
+    response_model = CardModel ,
     status_code=status.HTTP_200_OK,
 )
 async def play_card_endpoint(match_id, player_in_id, player_out_id, card_id):
@@ -128,7 +129,7 @@ async def play_card_endpoint(match_id, player_in_id, player_out_id, card_id):
     live_match = get_live_match_by_id(match_id)
     print(live_match)
 
-    card_random = CardModel
+    card_random = CardModel(id=0, name="", image="")
 
     if is_investigation_card(card):
         card_random = play_card_investigation(player_out,card)
@@ -144,7 +145,7 @@ async def play_card_endpoint(match_id, player_in_id, player_out_id, card_id):
         status = play_card(player_in, player_out, match_id, card_id)
     
     # send message to all players of the card played
-    msg_ws = create_ws_message(match_id, status, player_in.id, player_out.id)
+    msg_ws = create_ws_message(match_id, status, player_in.id, player_out_id)
     await live_match._match_connection_manager.broadcast_json(msg_ws)
 
     next_turn(match_id)
