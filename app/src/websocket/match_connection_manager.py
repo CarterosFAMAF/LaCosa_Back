da400e0c64c14fc8c40fc45e9a3fdf0663556643
Mario Picasso <mariopicasso@mi.unc.edu.ar>
2023-10-20 00:14:57 -0300

    send players sorted by position via ws

--- app/src/websocket/match_connection_manager.py
+++ app/src/websocket/match_connection_manager.py
@@ -147,9 +147,7 @@ def create_ws_message(
             )
         # sort players by turn
         players.sort(key=lambda x: x["turn"])
-        msg = get_ws_message_with_status(
-            status, player_name, player_target_name, card_name
-        )
+        msg = get_ws_message_with_status(status, player_name, player_target_name,card_name)
 
         match_ws = {
             "player_id": player_id,
