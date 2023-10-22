6f45d63c910deb11faf47e9d59bc3c60a4e6b573
RAQrodrigo <rodrigo.quevedo@mi.unc.edu.ar>
2023-10-22 17:48:18 -0300

    update

--- app/src/game/card.py
+++ app/src/game/card.py
@@ -32,11 +32,11 @@ def play_card(player_in, player_out, match_id: int, card_id: int):
     if card.card_id == LANZALLAMAS:
         status = play_lanzallamas(player_out.id)
     if card.card_id == MAS_VALE_QUE_CORRAS:
-        status = play_mas_vale_que_corras(player_in.id, player_out.id)
+        status = play_mas_vale_que_corras(player_in.id, player_out.id,match_id)
     if card.card_id == VIGILA_TUS_ESPALDAS:
         status = play_vigila_tus_espaldas(match_id)
     if card.card_id == CAMBIO_DE_LUGAR:
-        status = play_cambio_de_lugar(player_in.id,player_out.id)
+        status = play_cambio_de_lugar(player_in.id,player_out.id,match_id)
     else:
         pass
 
@@ -62,7 +62,7 @@ def play_lanzallamas(player_target_id):
     return status
 
 
-def play_mas_vale_que_corras(player_main_id, player_target_id):
+def play_mas_vale_que_corras(player_main_id, player_target_id,match_id):
     """
     Change the position of two players, and change the turn of the match
 
@@ -79,13 +79,13 @@ def play_mas_vale_que_corras(player_main_id, player_target_id):
         pos_tmp = player_main.position
         player_main.position = player_target.position
         player_target.position = pos_tmp
-        match = get_match_by_id(player_main.match)
+        match = get_match_by_id(match_id)
         match.turn = player_main.position
         flush()
     status = WS_STATUS_CHANGED_OF_PLACES
     return status
 
-def play_cambio_de_lugar(player_main_id, player_target_id):
+def play_cambio_de_lugar(player_main_id, player_target_id,match_id):
     
     with db_session:
         player_main = get_player_by_id(player_main_id)
@@ -93,7 +93,7 @@ def play_cambio_de_lugar(player_main_id, player_target_id):
         pos_tmp = player_main.position
         player_main.position = player_target.position
         player_target.position = pos_tmp
-        match = get_match_by_id(player_main.match)
+        match = get_match_by_id(match_id)
         match.turn = player_main.position
         flush()
     status = WS_STATUS_CHANGED_OF_PLACES
