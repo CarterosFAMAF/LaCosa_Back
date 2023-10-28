61b43c2d74d46b0da95597565fb93d2f4441aee4
RAQrodrigo <rodrigo.quevedo@mi.unc.edu.ar>
2023-10-28 19:36:06 -0300

    update

--- app/src/models/base.py
+++ app/src/models/base.py
@@ -1,6 +1,7 @@
 from pony.orm import *
 from app.src.game.constants import *
 import random
+
 db = Database()
 
 
@@ -54,11 +55,16 @@ def define_database_and_entities(test: bool):
     db.generate_mapping(create_tables=True)
 
 
-#imagenes para infeccion
+# imagenes para infeccion
+
 
 @db_session
 def load_cards():
-    image_infected = ["app/cards/Infeccion_1.png", "app/cards/Infeccion_2.png","app/cards/Infeccion_3.png"]
+    image_infected = [
+        "app/cards/Infeccion_1.png",
+        "app/cards/Infeccion_2.png",
+        "app/cards/Infeccion_3.png",
+    ]
     try:
         exists_card = db.exists("select * from Card where name='lanzallamas'")
         if not exists_card:
@@ -73,44 +79,59 @@ def load_cards():
                 image="app/cards/LaCosa.png",
             )
             Card(
-                card_id = MAS_VALE_QUE_CORRAS,
+                card_id=MAS_VALE_QUE_CORRAS,
                 name="Mas_Vale_Que_Corras",
                 image="app/cards/Mas_vale_que_corras.png",
             )
             Card(
-                card_id = SOSPECHA,
+                card_id=SOSPECHA,
                 name="Sospecha",
                 image="app/cards/sospecha.png",
             )
             Card(
-                card_id = VIGILA_TUS_ESPALDAS,
+                card_id=VIGILA_TUS_ESPALDAS,
                 name="Vigila_Tus_Espaldas",
                 image="app/cards/Vigila_tus_espaldas.png",
             )
             Card(
-                card_id = WHISKY,
+                card_id=WHISKY,
                 name="Whisky",
                 image="app/cards/Whisky.png",
             )
             Card(
-                card_id = ANALISIS,
+                card_id=ANALISIS,
                 name="Analisis",
                 image="app/cards/Analisis.png",
             )
             Card(
-                card_id = CAMBIO_DE_LUGAR,
+                card_id=CAMBIO_DE_LUGAR,
                 name="Cambio_de_lugar",
                 image="app/cards/Cambio_de_lugar.png",
             )
             Card(
-                card_id = SEDUCCION,
+                card_id=SEDUCCION,
                 name="Seduccion",
                 image="app/cards/Seduccion.png",
             )
             Card(
-                card_id = INFECCION,
+                card_id=UPS,
+                name="Ups!",
+                image="app/cards/Ups!.png",
+            )
+            Card(
+                card_id=CITA_A_CIEGAS,
+                name="Cita_a_ciegas",
+                image="app/cards/Cita_a_ciegas.png",
+            )
+            Card(
+                card_id=QUE_QUEDE_ENTRE_NOSOTROS,
+                name="Que_quede_entre_nosotros",
+                image="app/cards/Que_quede_entre_nosotros.png",
+            )
+            Card(
+                card_id=INFECCION,
                 name="Infeccion",
-                image= random.choice(image_infected),
+                image=random.choice(image_infected),
             )
             flush()
     except:
